be90ac87f3bd21c787e402842f22e0d2c0eea6cd
artem <artem.fediai@kit.edu>
2021-11-18 18:46:10 +0100

    creates sh file if requested

--- main1.py
+++ main1.py
@@ -3,6 +3,9 @@ import numpy as np
 import yaml
 
 global a, SETTINGS_FILENAME, DOP, N_R, N_DOP, N_CPUs, YAML_FILENAME
+#if I want sh file to be generated -->
+GENERATE_SH_FILE = True
+#<-- if I want sh file to be generated
 
 SETTINGS_FILENAME = 'settings_dop'  # LF settings file
 YAML_FILENAME = f'{__file__.split(".")[0]}.yaml'  #
@@ -15,6 +18,76 @@ a = np.array((Np_I_wish / DOP) ** (1.0 / 3.0), dtype=np.int)
 for i in range(len(a)):
     if a[i] < 15:
         a[i] = 15.0
+
+def generate_sh_file(file_name = 'run_lf_from_threadfarm.sh', 
+                     nodes=1, 
+                     time='0-24:00:00', 
+                     partition='accelerated',
+                     email='artem.fedyay@gmail.com',
+                     NANOMATCH='/home/hk-project-virtmat/bh5670/QP_installation_V4/nanomatch',
+                     NANOVER='V4',
+                     THREADFARM='/home/hk-project-virtmat/bh5670/QP_installation_V4/nanomatch/V4/QuantumPatch/mpithreadfarm/bin',
+                     ):
+    """
+    writes sh file to be run at horeka
+    :param file_name: sh file name tp generate
+    :param nodes: number nodes to use
+    :param time: sim time XX-XX:XX:XX
+    :param partition:
+    :param email:
+    :param NANOMATCH: path to nm env
+    :param NANOVER: version of it
+    :param THREADFARM: path to threadfarm
+    :return:
+    """
+    with open(file_name, 'a') as sh_file:
+        sh_file.write(f'#!/bin/bash'
+                      f'\n'
+                      f'# SBATCH --nodes={nodes}'
+                      f'\n'
+                      f'# SBATCH --time={time}'
+                      f'\n'
+                      f'# SBATCH --mem-per-cpu=2000mb'
+                      f'\n'
+                      f'# SBATCH --job-name=Lightforge'
+                      f'\n'
+                      f'# SBATCH --partition={partition}'
+                      f'\n'
+                      f'# SBATCH --mail-type=ALL'
+                      f'\n'
+                      f'# SBATCH --mail-user={email}'
+                      f'\n'
+                      f'# SBATCH --error out_%j.err'
+                      f'\n'
+                      f'# SBATCH --output out_%j.out'
+                      f'\n'
+                      f'\n'
+                      f'export NANOMATCH={NANOMATCH}'
+                      f'\n'
+                      f'export NANOVER={NANOVER}'
+                      f'\n'
+                      f'source $NANOMATCH/$NANOVER/configs/lightforge.config'
+                      f'\n'
+                      f'\n'
+                      f'export THREADFARM={THREADFARM}'
+                      f'\n'
+                      f'export THREADFARMBIN={THREADFARM}/bin'
+                      f'\n'
+                      f'export PYTHONPATH=$THREADFARM:$PYTHONPATH'
+                      f'\n'
+                      f'export PATH=$THREADFARM:$PATH'
+                      f'\n'
+                      f'module load compiler/gnu/11'
+                      f'\n'  # <-- load additional module. Needed?
+                      f'cd SLURM_SUBMIT_DIR'
+                      f'\n'
+                      f'ulimit -s unlimited'
+                      f'\n'
+                      f'export KMP_AFFINITY=none'
+                      f'\n'
+                      f'\n'
+                      f'MPI_PATH/bin/mpirun --bind-to none --mca btl self,vader,tcp python $THREADFARMBIN/thread_mpi_exe.py joblist'
+                      f'\n')
 
 
 def run_kmc(i_dop, i_r):
@@ -77,6 +150,9 @@ def main():
     print("\nSettings hyperparameters:")
     for key, value in main_settings_dict.items():
         print(f'{key} \t-->\t {value}')
+
+    if GENERATE_SH_FILE:
+        generate_sh_file()
 
 if __name__ == '__main__':
     main()
